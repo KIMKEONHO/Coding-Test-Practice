83047da1ac8ee399010cd50c49bd9da63ba2ff45
KIMKEONHO <jerry6475@naver.com>
2025-12-16 20:04:56 +0900

    2025-12-16

---
+++ 괄호 짝 맞추기.py
@@ -0,0 +1,17 @@
+def solution(s):
+
+    stack = []
+
+    for c in s:
+        if c == '(':
+            stack.append(c)
+        elif c == ')':
+            if not stack:
+                return False
+            else:
+                stack.pop()
+
+    if stack:
+        return False
+    else:
+        return True
